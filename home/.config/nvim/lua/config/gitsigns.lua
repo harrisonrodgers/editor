cf7516eb1d9127f1f837d75409ac21c6be27ff33
Harrison Rodgers <hrod1137@uni.sydney.edu.au>
2024-10-09 17:52:18 +1100

    editor

--- home/.config/nvim/lua/config/gitsigns.lua
+++ home/.config/nvim/lua/config/gitsigns.lua
@@ -1,12 +1,27 @@
 -- Configure gitsigns (https://github.com/lewis6991/gitsigns.nvim)
 require("gitsigns").setup({
     signs = {
-        add = { hl = "GitSignsAdd", text = " ", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-        change = { hl = "GitSignsChange", text = " ", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-        delete = { hl = "GitSignsDelete", text = " ", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-        topdelete = { hl = "GitSignsDelete", text = " ", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-        changedelete = { hl = "GitSignsDelete", text = " ", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+        add = { text = " " },
+        change = { text = " " },
+        delete = { text = " " },
+        topdelete = { text = " " },
+        changedelete = { text = " " },
     },
+    vim.api.nvim_set_hl(0, 'GitSignsAdd', { link = 'GitSignsAdd' }),
+    vim.api.nvim_set_hl(0, 'GitSignsAddLn', { link = 'GitSignsAddLn' }),
+    vim.api.nvim_set_hl(0, 'GitSignsAddNr', { link = 'GitSignsAddNr' }),
+    vim.api.nvim_set_hl(0, 'GitSignsChange', { link = 'GitSignsChange' }),
+    vim.api.nvim_set_hl(0, 'GitSignsChangeLn', { link = 'GitSignsChangeLn' }),
+    vim.api.nvim_set_hl(0, 'GitSignsChangeNr', { link = 'GitSignsChangeNr' }),
+    vim.api.nvim_set_hl(0, 'GitSignsChangedelete', { link = 'GitSignsDelete' }),
+    vim.api.nvim_set_hl(0, 'GitSignsChangedeleteLn', { link = 'GitSignsDeleteLn' }),
+    vim.api.nvim_set_hl(0, 'GitSignsChangedeleteNr', { link = 'GitSignsDeleteNr' }),
+    vim.api.nvim_set_hl(0, 'GitSignsDelete', { link = 'GitSignsDelete' }),
+    vim.api.nvim_set_hl(0, 'GitSignsDeleteLn', { link = 'GitSignsDeleteLn' }),
+    vim.api.nvim_set_hl(0, 'GitSignsDeleteNr', { link = 'GitSignsDeleteNr' }),
+    vim.api.nvim_set_hl(0, 'GitSignsTopdelete', { link = 'GitSignsDelete' }),
+    vim.api.nvim_set_hl(0, 'GitSignsTopdeleteLn', { link = 'GitSignsDeleteLn' }),
+    vim.api.nvim_set_hl(0, 'GitSignsTopdeleteNr', { link = 'GitSignsDeleteNr' }),
     on_attach = function(bufnr)
         local gitsigns = require('gitsigns')
         local bufopts = { noremap = true, silent = true, buffer = bufnr }
